a6482c41221310a5f3e838cdb2e6c3c95c4722eb
Mugisha Abdoul Latif <85370865+MugAbdoul@users.noreply.github.com>
2025-10-15 21:17:52 +0200

    update userAuth

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,6 +2,16 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../store/store';
 
 export const useAuth = () => {
-  const { isAuthenticated, roles, user, accessToken, status, error } = useSelector((state: RootState) => state.auth);
-  return { isAuthenticated, roles, user, token: accessToken, status, error };
+  const { isAuthenticated, roles, user, accessToken, status, error } = useSelector(
+    (state: RootState) => state.auth
+  );
+
+  return {
+    isAuthenticated: Boolean(isAuthenticated),
+    roles: roles ?? [], // ✅ fallback to empty array
+    user,
+    token: accessToken,
+    status,
+    error,
+  };
 };
